936aabce7aba400895a3da1372c51a0bcb150e17
SK IMRAN <120629426+IMU-IMRAN@users.noreply.github.com>
2024-05-27 19:31:42 +0000

    variables and datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3)  //we are using nodejs , not browser
+
+let name ="imran"
+let age=19
+let isLoggedIn= false
+let state;
+
+//*primitive datatype
+//number=> 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//*Object
+console.log(typeof undefined)  // type is also undefined
+console.log(typeof null) // type is object
