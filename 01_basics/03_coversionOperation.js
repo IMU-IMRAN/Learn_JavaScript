a3d7394342db271da106c992ff128b53a23e2247
SK IMRAN <120629426+IMU-IMRAN@users.noreply.github.com>
2024-05-27 20:55:38 +0000

    Operations

--- 01_basics/03_coversionOperation.js
+++ 01_basics/03_coversionOperation.js
@@ -16,4 +16,26 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber =33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+//console.log(stringNumber);
+
+
+//****************Operations********************/
+let value =3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2+2);
+// console.log(2**2);
+
+let str1 = "sk"
+let str2 = "imran"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+1+"2");
+console.log("2"+1+1);
+console.log(+true);
+console.log(+"");
